95261bcf2d36c4ecec98b9c66193310f7ec8300d
sstack124 <Tara658865>
2022-08-21 14:37:47 -0400

    Testing Some user stuff

--- application.py
+++ application.py
@@ -7,10 +7,16 @@ application = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-
+'''
 @application.route('/user', methods=['GET', 'POST'])
 def view_user():
     if request.method == 'GET':
         return "Hello, I dont know what your name is"
     if request.method == 'POST':
-        return json.dumps({'name' : request.form['name']})
+        return json.dumps({'name': request.form['name']})
+'''
+
+
+@application.route('/user', methods=['POST'])
+def view_user():
+    return json.dumps({'name': request.form['name']})
